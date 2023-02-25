b9ccec015a8800fb132b97a687c81fa82843724a
Kenny Cordero <GeckoB4I@gmail.com>
2023-02-25 12:33:23 -0600

    fixed the links to GH and deployment form the porfolio page

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -6,15 +6,15 @@ export default function Portfolio() {
     }, [])
 
     const workProps = [
-        { projectName: "Be Tethical", projectTechnologies: "Sequelize, JavaScript", link: "https://github.com/Kenny4297/Be-Tethical" },
+        { projectName: "Be Tethical (GitHub)", githubLink: 'https://github.com/Kenny4297/Be-Tethical' },
     
-        { projectName: "Note Taker App", projectTechnologies: "JavaScript, Express.js", link: "https://zealous-wound-production.up.railway.app/" },
+        { projectName: "Note Taker App (GitHub)", githubLink: 'https://github.com/Kenny4297/Taking-Some-Notes', deploymentLink: "https://zealous-wound-production.up.railway.app/" },
     
-        { projectName: "E-Commerce", projectTechnologies: "Node.js, JavaScript, SQL", link: "https://github.com/Kenny4297/Electronic-Commerse-DB"},
+        { projectName: "E-Commerce (GitHub)", githubLink: "https://github.com/Kenny4297/Electronic-Commerse-DB"},
     
-        { projectName: "Employee Tracker", projectTechnologies: "Node.js, JavaScript, SQL", link: "https://github.com/Kenny4297/Track-that-Employee"},
+        { projectName: "Employee Tracker (GitHub)", githubLink: "https://github.com/Kenny4297/Track-that-Employee"},
     
-        { projectName: "Weather Dashboard", projectTechnologies: "Open Weather API, JavaScript, Bootstrap", link: "https://kenny4297.github.io/Keds-Wicked-Weather-WApp/"}
+        { projectName: "Weather Dashboard (GitHub)", githubLink: "https://kenny4297.github.io/Keds-Wicked-Weather-WApp/", deploymentLink: ""}
     ];
 
     return (
@@ -24,12 +24,10 @@ export default function Portfolio() {
                 {/* Here we loop through the props located below this function.  */}
                 {workProps.map((individualWork, index) => (
                     // We include the index as a second parameter to know what specific project we are on for CSS purposes
-                    
                         <div key={index} className={`grid-item work-example-${index + 1}`}>
-                            <a href={individualWork.link} target="_blank" rel="noreferrer">
-                                <h3 className='project-title'>{individualWork.projectName}</h3>
-                                <p className='project-technologies'>{individualWork.projectTechnologies}</p>
-                            </a>  
+                            <h3 className='project-technologies'><a href={individualWork.githubLink} target="_blank" rel="noreferrer">{individualWork.projectName}</a></h3>
+                            <p className='project-technologies'>{individualWork.projectTechnologies}</p>
+                            {index === 1 && <p className='project-technologies'><a href={individualWork.deploymentLink} target="_blank" rel="noreferrer">Deployment</a></p>}
                         </div>
                 ))}
                 {/* Notice how this section closing tag is outside the map! We are only mapping through the specific work section, not the entire section itself */}
